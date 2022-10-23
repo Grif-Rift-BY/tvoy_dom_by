b2abd38f9955a938fff78437db44dcf99311b367
Iryna Glytsko <82415832+IrynaGlytsko@users.noreply.github.com>
2022-10-23 16:11:43 +0300

    feat: update file

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ $(document).ready(function(){
         return false;
     });
 });
+
+
+/* Slick слайдер */
+
+$(function() {
+    $('.reviews__profile').slick();
+});
+
+/* Pop up */
+
+let button = document.getElementById('open');
+let close = document.getElementById('win');
+
+button.addEventListener('click', function(event){
+    event.preventDefault();
+    modal.style.display = 'block';
+});
+
+close.addEventListener('click', function(event){
+    event.preventDefault();
+    modal.style.display = 'none';
+});
