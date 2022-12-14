49454bc263c4e596a18f3d1b609dd5d25f153ac5
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-12-14 04:14:15 +0300

    refactor: a few small changes

--- js/email-send.js
+++ js/email-send.js
@@ -1,27 +1,27 @@
 
-  $( '#mail-form' ).on( 'submit', ( event ) => {
-    event.preventDefault();
-    let dataPackage = $( '#mail-form' ).serializeArray();
-    $.ajax( {
-      url: "../php/sendmail.php",
-      type: "POST",
-      data: dataPackage,
-      success: ( response ) => {
-        if ( response == 'OK' ) {
-            $( '#mail-form' )[0].reset();
-            formVisibilityToggler();
-        };
-      }
-    } );
+$( '#mail-form' ).on( 'submit', ( event ) => {
+  event.preventDefault(); // Предотвращаем отправку формы по умолчанию.
+  const dataPackage = $( '#mail-form' ).serializeArray(); // Собираем данные с полей формы
+  $.ajax( {
+    url: "../php/sendmail.php", // Путь к скрипту отправки письма
+    type: "POST",
+    data: dataPackage,
+    success: ( response ) => {
+      if ( response == 'OK' ) {
+          $( '#mail-form' )[0].reset(); // Сброс формы
+          formVisibilityToggler(); // Переключение формы для показа подтверждения
+      };
+    }
   } );
-  $('#form-button-ok').on('click',( event ) =>{
-    formVisibilityToggler();
-    modal.style.display = 'none';
-  })
+} );
+$('#form-button-ok').on('click',() =>{
+  formVisibilityToggler(); // Переключение формы к начальному виду.
+  modal.style.display = 'none'; // Закрытие формы по нажатию кнопки ОК
+})
 
-  function formVisibilityToggler() {
-    $( '#form-response' ).toggleClass('form__element-hidden');
-    $( '#form-button-ok' ).toggleClass('form__element-hidden');
-    $( '.user-box' ).toggleClass('form__element-hidden');
-    $( '#form-button-submit' ).toggleClass('form__element-hidden');
-  }
+function formVisibilityToggler() {
+  $( '#form-response' ).toggleClass('form__element-hidden');
+  $( '#form-button-ok' ).toggleClass('form__element-hidden');
+  $( '.user-box' ).toggleClass('form__element-hidden');
+  $( '#form-button-submit' ).toggleClass('form__element-hidden');
+}
